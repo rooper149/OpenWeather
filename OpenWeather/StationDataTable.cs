bff356c1730c68299e537034eedb300650efb77a
Ryan S. Cooper <cooper.ryan@centaurisoftware.co>
2016-06-07 21:25:55 -0500

    Changed station list storage to project resource.

---
+++ OpenWeather/StationDataTable.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using OpenWeather.Properties;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+#if !ANDROID
+using System.Device.Location;
+#else
+using Android.Locations;
+#endif
+
+namespace OpenWeather
+{
+    public class StationDataTable
+    {
+        public DataTable Stations { get; }
+
+        public StationDataTable()
+        {
+            Stations = new DataTable();
+
+            Stations.Columns.Add("ICAO", typeof(string));
+            Stations.Columns.Add("Latitude", typeof(double));
+            Stations.Columns.Add("Longitude", typeof(double));
+            Stations.Columns.Add("Elevation", typeof(double));
+            Stations.Columns.Add("Country", typeof(string));
+            Stations.Columns.Add("Region", typeof(string));
+            Stations.Columns.Add("City", typeof(string));
+
+            using (var reader = new StringReader(Resources.official_stations))
+            {
+                string[] fields;
+                while ((fields = reader.ReadLine()?.Split(',')) != null)
+                {
+                    Debug.Assert(fields != null, "official_stations list has an error");
+                    Stations.Rows.Add(fields[0], Convert.ToDouble(fields[1]), Convert.ToDouble(fields[2]),
+                        Convert.ToDouble(fields[3]), fields[4], fields[5], fields[6]);
+                }
+            }
+        }
+
+        public Station GetStation(string icao)
+        {
+            var row = Stations.AsEnumerable()
+                .SingleOrDefault(r => r.Field<string>("ICAO") == icao);
+
+            return new Station((string) row["ICAO"], (double) row["Latitude"], (double) row["Longitude"],
+                (double) row["Elevation"],
+                (string) row["Country"], (string) row["Region"], (string) row["City"]);
+        }
+
+        public Station GetClosestStation(double latitude, double longitude)
+        {
+
+            var rows = Stations.Rows.Cast<DataRow>().ToList();
+            var closestStation = new Station((string) rows[0]["ICAO"], (double) rows[0]["Latitude"],
+                (double) rows[0]["Longitude"], (double) rows[0]["Elevation"],
+                (string) rows[0]["Country"], (string) rows[0]["Region"], (string) rows[0]["City"]);
+
+#if !ANDROID
+            var location = new GeoCoordinate(latitude, longitude);
+
+            foreach (var row in from row in rows.Skip(1) let dest = new GeoCoordinate((double)row["Latitude"], (double)row["Longitude"])
+                                where location.GetDistanceTo(dest) < location.GetDistanceTo(closestStation.Location) select row)
+                closestStation = new Station((string) row["ICAO"], (double) row["Latitude"],
+                    (double) row["Longitude"], (double) row["Elevation"],
+                    (string) row["Country"], (string) row["Region"], (string) row["City"]);
+#else
+            var location = new Location("ORGIN")
+            {
+                Latitude = latitude,
+                Longitude = longitude
+            };
+
+            foreach (var row in from row in rows
+                let possibleEndPoint = new Location("DEST_P")
+                {
+                    Latitude = (double) row["Latitude"],
+                    Longitude = (double) row["Longitude"]
+                }
+                let currentEndPoint = new Location("DEST")
+                {
+                    Latitude = closestStation.Location.Latitude,
+                    Longitude = closestStation.Location.Logitude
+                }
+                where location.DistanceTo(possibleEndPoint) < location.DistanceTo(currentEndPoint)
+                select row)
+                closestStation = new Station((string) row["ICAO"], (double) row["Latitude"],
+                    (double) row["Longitude"], (double) row["Elevation"],
+                    (string) row["Country"], (string) row["Region"], (string) row["City"]);
+#endif
+            return closestStation;
+        }
+    }
+}
--- OpenWeather/StationLookup.cs
+++ OpenWeather/StationLookup.cs
@@ -1,71 +1,33 @@
-﻿//#define USE_PERSISTANT_LIST
-
+﻿#define USE_PERSISTANT_LIST
 using System;
-using System.Collections.Generic;
-using System.Linq;
-#if !ANDROID
-using System.Device.Location;
-#else
-using Android.Locations;
-#endif
 
 namespace OpenWeather
 {
-    public class StationList
+    public class StationLookup
     {
-        private static readonly Lazy<StationList> _lazy =
-            new Lazy<StationList>(() => new StationList());
+        private static readonly Lazy<StationLookup> _lazy =
+            new Lazy<StationLookup>(() => new StationLookup());
 
-        private StationList() {}
-        public static StationList Instance => _lazy.Value;
+        private StationLookup() {}
+        public static StationLookup Instance => _lazy.Value;
 
 #if USE_PERSISTANT_LIST
-        private readonly List<Station> stations = new StationCollection().Stations;
+        private readonly StationDataTable stations = new StationDataTable();
 #endif
         public Station Lookup(string icao)
         {
-
 #if !USE_PERSISTANT_LIST
-            var stations = new StationCollection().Stations;
+            var stations = new StationDataTable();
 #endif
-
-            return stations.FirstOrDefault(s => s.ICAO == icao);
+            return stations.GetStation(icao);
         }
 
         public Station Lookup(double latitude, double longitude)
         {
 #if !USE_PERSISTANT_LIST
-            var stations = new StationCollection().Stations;
+            var stations = new StationDataTable();
 #endif
-#if !ANDROID
-            var location = new GeoCoordinate(latitude, longitude);
-#else
-        var location = new Location("ORGIN");
-            location.Latitude = latitude;
-            location.Longitude = longitude;
-#endif
-
-            var closestStation = stations[0];
-
-#if !ANDROID
-            foreach (var station in stations.Where(station => location.GetDistanceTo(station.Location) < location.GetDistanceTo(closestStation.Location)))
-                closestStation = station;
-#else
-            foreach (var station in stations)
-            {
-                var possibleEndPoint = new Location("DEST_P");
-                possibleEndPoint.Latitude = station.Location.Latitude;
-                possibleEndPoint.Longitude = station.Location.Logitude;
-
-                var currentEndPoint = new Location("DEST");
-                currentEndPoint.Latitude = closestStation.Location.Latitude;
-                currentEndPoint.Longitude = closestStation.Location.Logitude;
-
-                if (location.DistanceTo(possibleEndPoint) < location.DistanceTo(currentEndPoint))
-                    closestStation = station;
-            }
-#endif
-            return closestStation;
+            return stations.GetClosestStation(latitude, longitude);
         }
     }
 }
