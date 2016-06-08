1808804e5bd7f4cb4ce03100db54fa0106f0de61
Ryan S. Cooper <cooper.ryan@centaurisoftware.co>
2016-06-08 05:29:15 -0500

    added SetPersistentLookup to include GC.Collect call

--- OpenWeather/StationLookup.cs
+++ OpenWeather/StationLookup.cs
@@ -1,5 +1,4 @@
-﻿#define USE_PERSISTANT_LIST
-using System;
+﻿using System;
 
 namespace OpenWeather
 {
@@ -8,26 +7,24 @@ namespace OpenWeather
         private static readonly Lazy<StationLookup> _lazy =
             new Lazy<StationLookup>(() => new StationLookup());
 
-        private StationLookup() {}
+        private StationLookup() { stations = new StationDataTable(); }
         public static StationLookup Instance => _lazy.Value;
 
-#if USE_PERSISTANT_LIST
-        private readonly StationDataTable stations = new StationDataTable();
-#endif
-        public Station Lookup(string icao)
-        {
-#if !USE_PERSISTANT_LIST
-            var stations = new StationDataTable();
-#endif
-            return stations.GetStation(icao);
-        }
+        private bool isPersistent = true;
+        private StationDataTable stations;
 
-        public Station Lookup(double latitude, double longitude)
+        public Station Lookup(string icao) => !isPersistent ? new StationDataTable().GetStation(icao) : stations.GetStation(icao);
+
+        public Station Lookup(double latitude, double longitude) => !isPersistent ? new StationDataTable().GetClosestStation(latitude, longitude)
+            : stations.GetClosestStation(latitude, longitude);
+
+        public void SetPersistentLookup(bool val)
         {
-#if !USE_PERSISTANT_LIST
-            var stations = new StationDataTable();
-#endif
-            return stations.GetClosestStation(latitude, longitude);
+            isPersistent = val;
+            stations = val ? new StationDataTable() : null;
+
+            if(!val)
+                GC.Collect();
         }
     }
 }
