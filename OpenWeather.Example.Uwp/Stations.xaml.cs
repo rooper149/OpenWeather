1452bd1dec131bb4c495294aaff062c303203756
iti4dmv\rthiele <rthiele@iti4dmv.com>
2018-01-04 13:36:00 -0500

    Added Stations to example.

---
+++ OpenWeather.Example.Uwp/Stations.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace OpenWeather.Example.Uwp
+{
+    public sealed partial class Stations : UserControl
+    {
+        public Stations()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void UserControl_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            NoaaApi api = new NoaaApi(null);
+            var stations = await api.GetStationsAsync();
+
+            this.cvs.Source = stations.OrderBy(x => x.Name).GroupBy(x => x.Name.Substring(0, 1));
+        }
+    }
+}
