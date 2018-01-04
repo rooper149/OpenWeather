ba717b8a473653d0b6d46aa617e5999d2deeab0d
iti4dmv\rthiele <rthiele@iti4dmv.com>
2018-01-04 14:58:01 -0500

    Added stations storage.

--- OpenWeather.Core/StationFileParser.cs
+++ OpenWeather.Core/StationFileParser.cs
@@ -19,11 +19,6 @@ namespace OpenWeather
         internal async Task<IEnumerable<Station>> GetStationsAsync()
         {
             string stationsFileContent = null;
-
-            //// check if the file exists
-            //if (await Task.FromResult(File.Exists("Stations.txt")) == false)
-            //{
-            //    // if the file doesn't exist, download it.
             using (HttpClient client = new HttpClient())
             {
                 using (HttpResponseMessage response = await client.GetAsync(_uri))
@@ -31,93 +26,81 @@ namespace OpenWeather
                     if (response.IsSuccessStatusCode)
                     {
                         stationsFileContent = await response.Content.ReadAsStringAsync();
-
-                        //FileInfo fileIo = new FileInfo("Stations.txt");
-                        //using (FileStream fs = new FileStream("Stations.txt", FileMode.Create, FileAccess.Write))
-                        //{
-                        //    using (StreamWriter writer = new StreamWriter(fs))
-                        //    {
-                        //        using (StreamReader reader = new StreamReader(await response.Content.ReadAsStreamAsync()))
-                        //        {
-                        //            await writer.WriteAsync(await reader.ReadToEndAsync());
-                        //        }
-                        //    }
-                        //}
-
                     }
                 }
             }
-            //}
 
+            return await ParseStationsAsync(stationsFileContent);
+        }
+
+
+        private async Task<IEnumerable<Station>> ParseStationsAsync(string stationsFileContent)
+        {
             List<Station> stations = new List<Station>();
-            // we now have the file. Parse it.
-            //using (FileStream fs = new FileStream("Stations.txt", FileMode.Open, FileAccess.Read))
-            //{
-                using (StringReader reader = new StringReader(stationsFileContent))
+
+            using (StringReader reader = new StringReader(stationsFileContent))
+            {
+                while (reader.Peek() != -1)
                 {
-                    while (reader.Peek() != -1)
-                    {
-                        string line = await reader.ReadLineAsync();
-                        // fileter comments
-                        if (line.StartsWith("!")) continue;
-                        // fileter short lines
-                        if (line.Length < 83) continue;
+                    string line = await reader.ReadLineAsync();
+                    // fileter comments
+                    if (line.StartsWith("!")) continue;
+                    // fileter short lines
+                    if (line.Length < 83) continue;
 
-                        // we have a potientally valid line.
-                        Station station = new Station();
-                        string synopValue = null;
-                        string latitudeValue = null;
-                        string longitudeValue = null;
-                        string elevationValue = null;
-                        string priorityValue = null;
+                    // we have a potientally valid line.
+                    Station station = new Station();
+                    string synopValue = null;
+                    string latitudeValue = null;
+                    string longitudeValue = null;
+                    string elevationValue = null;
+                    string priorityValue = null;
 
-                        if (line.Length >= 2) station.StateOrProvince = line.Substring(0, 2).Trim();
-                        if (line.Length >= 19) station.Name = line.Substring(3, 16).Trim();
-                        if (line.Length >= 25) station.ICAO = line.Substring(20, 4).Trim();
-                        if (line.Length >= 30) station.IATA = line.Substring(26, 3).Trim();
-                        if (line.Length >= 38) synopValue = line.Substring(32, 5).Trim();
-                        if (line.Length >= 45) latitudeValue = line.Substring(39, 7).Trim();
-                        if (line.Length >= 53) longitudeValue = line.Substring(47, 7).Trim();
-                        if (line.Length >= 59) elevationValue = line.Substring(55, 4).Trim();
-                        if (line.Length >= 64) station.IsMetar = line.Substring(62, 2).Trim() == "X";
-                        if (line.Length >= 67) station.IsNexRad = line.Substring(65, 2).Trim() == "X";
-                        if (line.Length >= 70) station.AviationIndicator = line.Substring(68, 2).Trim();
-                        if (line.Length >= 73) station.UpperAiraIndicator = line.Substring(71, 2).Trim();
-                        if (line.Length >= 76) station.AutomationIndicator = line.Substring(74, 2).Trim();
-                        if (line.Length >= 79) station.OfficeTypeIndicator = line.Substring(77, 2).Trim();
-                        if (line.Length >= 81) priorityValue = line.Substring(79, 2).Trim();
-                        if (line.Length >= 83) station.CountryCode = line.Substring(81, 2).Trim();
+                    if (line.Length >= 2) station.StateOrProvince = line.Substring(0, 2).Trim();
+                    if (line.Length >= 19) station.Name = line.Substring(3, 16).Trim();
+                    if (line.Length >= 25) station.ICAO = line.Substring(20, 4).Trim();
+                    if (line.Length >= 30) station.IATA = line.Substring(26, 3).Trim();
+                    if (line.Length >= 38) synopValue = line.Substring(32, 5).Trim();
+                    if (line.Length >= 45) latitudeValue = line.Substring(39, 7).Trim();
+                    if (line.Length >= 53) longitudeValue = line.Substring(47, 7).Trim();
+                    if (line.Length >= 59) elevationValue = line.Substring(55, 4).Trim();
+                    if (line.Length >= 64) station.IsMetar = line.Substring(62, 2).Trim() == "X";
+                    if (line.Length >= 67) station.IsNexRad = line.Substring(65, 2).Trim() == "X";
+                    if (line.Length >= 70) station.AviationIndicator = line.Substring(68, 2).Trim();
+                    if (line.Length >= 73) station.UpperAiraIndicator = line.Substring(71, 2).Trim();
+                    if (line.Length >= 76) station.AutomationIndicator = line.Substring(74, 2).Trim();
+                    if (line.Length >= 79) station.OfficeTypeIndicator = line.Substring(77, 2).Trim();
+                    if (line.Length >= 81) priorityValue = line.Substring(79, 2).Trim();
+                    if (line.Length >= 83) station.CountryCode = line.Substring(81, 2).Trim();
 
-                        int synop = 0;
-                        Int32.TryParse(synopValue, out synop);
-                        station.SYNOP = synop;
+                    int synop = 0;
+                    Int32.TryParse(synopValue, out synop);
+                    station.SYNOP = synop;
 
-                        double latitude = 0, longitude = 0, elevation = 0;
-                        latitudeValue = latitudeValue.Replace(" ", ".");
-                        longitudeValue = longitudeValue.Replace(" ", ".");
+                    double latitude = 0, longitude = 0, elevation = 0;
+                    latitudeValue = latitudeValue.Replace(" ", ".");
+                    longitudeValue = longitudeValue.Replace(" ", ".");
 
-                        double.TryParse(latitudeValue.Substring(0, latitudeValue.Length - 1), out latitude);
-                        if (latitudeValue.EndsWith("S")) latitude *= -1;
-                        station.Latitude = latitude;
+                    double.TryParse(latitudeValue.Substring(0, latitudeValue.Length - 1), out latitude);
+                    if (latitudeValue.EndsWith("S")) latitude *= -1;
+                    station.Latitude = latitude;
 
-                        double.TryParse(longitudeValue.Substring(0, longitudeValue.Length - 1), out longitude);
-                        if (longitudeValue.EndsWith("W")) longitude *= -1;
-                        station.Longitude = longitude;
+                    double.TryParse(longitudeValue.Substring(0, longitudeValue.Length - 1), out longitude);
+                    if (longitudeValue.EndsWith("W")) longitude *= -1;
+                    station.Longitude = longitude;
 
-                        double.TryParse(elevationValue, out elevation);
-                        station.Elevation = elevation;
+                    double.TryParse(elevationValue, out elevation);
+                    station.Elevation = elevation;
 
-                        int priority = 0;
-                        Int32.TryParse(priorityValue, out priority);
-                        station.Priority = priority;
+                    int priority = 0;
+                    Int32.TryParse(priorityValue, out priority);
+                    station.Priority = priority;
 
-                        stations.Add(station);
-                    }
+                    stations.Add(station);
                 }
-            //}
+            }
 
             return stations;
         }
-
     }
 }
--- OpenWeather.Example.Uwp/Stations.xaml.cs
+++ OpenWeather.Example.Uwp/Stations.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System.Linq;
+﻿using OpenWeather.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
+
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -15,10 +22,23 @@ namespace OpenWeather.Example.Uwp
 
         private async void UserControl_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            NoaaApi api = new NoaaApi(null);
-            var stations = await api.GetStationsAsync();
+            IEnumerable<Station> stations = null;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Station>));
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile storageFile = await storageFolder.CreateFileAsync("Stations.dat", CreationCollisionOption.OpenIfExists);
 
-            this.cvs.Source = stations.OrderBy(x => x.Name).GroupBy(x => x.Name.Substring(0, 1));
+            if (storageFile != null)
+            {
+                stations = (List<Station>)xmlSerializer.Deserialize(await storageFile.OpenStreamForReadAsync());
+            }
+            else
+            {
+                NoaaApi api = new NoaaApi(null);
+                stations = await api.GetStationsAsync();
+                xmlSerializer.Serialize(await storageFile.OpenStreamForWriteAsync(), stations);
+            }
+
+            cvs.Source = stations.OrderBy(x => x.Name).GroupBy(x => x.Name.Substring(0, 1));
         }
     }
 }
