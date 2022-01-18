8a2bf169291d16673d499d09ef335691e03e3679
Ryan S. Cooper <cooper.ryan@centaurisoftware.co>
2022-01-17 22:40:49 -0600

    Started work on refreshing this project. Added a tool to generate a C# class that has all the stations in a dictionary. There will be no need for CSV parsing anymore.

---
+++ OpenWeather.Data/StationInfo.cs
@@ -0,0 +1,54 @@
+﻿namespace OpenWeather.Data
+{
+    /// <summary>
+    /// Struct to hold station information
+    /// </summary>
+    public struct StationInfo
+    {
+        /// <summary>
+        /// Station ICAO code
+        /// </summary>
+        public string ICAO { get; set; }
+
+        /// <summary>
+        /// Station name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The elevation of the weather station in meters
+        /// </summary>
+        public int Elevation { get; set; }
+
+        /// <summary>
+        /// Two character coutry code
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Two character state/region code
+        /// </summary>
+        public string Region { get; set; }
+
+        /// <summary>
+        /// The latitude of the station
+        /// </summary>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// The logitude of the station
+        /// </summary>
+        public double Longitude { get; set; }
+
+        public StationInfo(string icao, string name, int elevation, string country, string region, double lat, double lon)
+        {
+            ICAO = icao;
+            Name = name;
+            Elevation = elevation;
+            Country = country;
+            Region = region;
+            Latitude = lat;
+            Longitude = lon;
+        }
+    }
+}
---
+++ Tools/StationListBuilder/Station.cs
@@ -0,0 +1,29 @@
+﻿namespace StationListBuilder
+{
+    public struct Station
+    {
+
+        public readonly string CD;      //2  chars
+        public readonly string STATION; //16 chars
+        public readonly string? ICAO;    //4  chars
+        public readonly string? IATA;   //3  chars
+        public readonly string? SYNOP;  //5  chars
+        public readonly string LAT;     //6  chars
+        public readonly string LON;     //7  chars
+        public readonly string ELEV;    //4  chars
+        public readonly string CC;      //2  chars
+
+        public Station(string line)
+        {
+            CD = line[..3].Trim();
+            STATION = line.Substring(3, 17).Trim();
+            ICAO = line.Substring(19, 5).Trim();
+            IATA = line.Substring(21, 3).Trim();
+            SYNOP = line.Substring(29, 8).Trim();
+            LAT = line.Substring(38, 8).Trim();
+            LON = line.Substring(45, 9).Trim();
+            ELEV = line.Substring(54, 5).Trim();
+            CC = line.Substring(line.Length - 2, 2);
+        }
+    }
+}
